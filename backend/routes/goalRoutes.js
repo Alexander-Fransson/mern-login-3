11909e0e92d3e0202700269854f2129936b7e38e
festusOp7 <70211809+festusOp7@users.noreply.github.com>
2022-08-09 20:28:03 +0200

    börjar med controllers

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -5,4 +5,15 @@ router.get('/', (req,res) => {
     res.status(200).json({message:'getting'});
 });
 
+router.post('/', (req,res) => {
+    res.status(200).json({message:'setting'});
+});
+router.put('/:id', (req,res) => {
+    res.status(200).json({message:'updating '+req.params.id});
+});
+
+router.delete('/:id', (req,res) => {
+    res.status(200).json({message:'deleting '+req.params.id});
+});
+
 module.exports = router;

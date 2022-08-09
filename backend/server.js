56ac56e6d8a41844044289730412a402904b8bec
festusOp7 <70211809+festusOp7@users.noreply.github.com>
2022-08-09 20:16:13 +0200

    skapar grund api och routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log('hio');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT;
+
+const app = express();
+
+app.get('/api/goals', (req,res) => {
+    res.status(200).json({message:'getting'});
+});
+
+app.listen(port,() => console.log('listening'+port));

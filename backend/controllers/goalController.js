ca01d7dca1004a21dc8c000cc1d6625a0609b719
festusOp7 <70211809+festusOp7@users.noreply.github.com>
2022-08-13 22:22:09 +0200

    getting the goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -25,17 +25,15 @@ const putGoals = asyncHandler(async (req,res) => {
         res.status(400);
         throw new Error('Goal not found');
     }
-
-    const user = await User.findById(req.user.id);
     
     //check for user
-    if(!user){
+    if(!req.user){
         res.status(401);
         throw new Error('User not found')
     }
 
     //Make sure the logged in user matches the goal user
-    if(goal.user.toString() !== user.id){
+    if(goal.user.toString() !== req.user.id){
         res.status(401);
         throw new Error('User not authorized');
     }
@@ -52,17 +50,15 @@ const deleteGoals = asyncHandler(async (req,res) => {
         res.status(400);
         throw new Error('Goal not found');
     }
-
-    const user = await User.findById(req.user.id);
     
     //check for user
-    if(!user){
+    if(!req.user){
         res.status(401);
         throw new Error('User not found')
     }
 
     //Make sure the logged in user matches the goal user
-    if(goal.user.toString() !== user.id){
+    if(goal.user.toString() !== req.user.id){
         res.status(401);
         throw new Error('User not authorized');
     }
